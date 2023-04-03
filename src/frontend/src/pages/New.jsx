992e0520f424f7db71c723d8bfdc5a8b9f9f76b8
Franco Balich <franco.balich@gmail.com>
2023-04-03 16:09:52 -0300

    Create New ToDo

---
+++ src/frontend/src/pages/New.jsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+export const NewTodo = () => {
+  return (
+    <>
+    <input className="browser__input" type="text" placeholder="Ingrese el nombre de la tarea"/>
+    </>
+  )
+}
