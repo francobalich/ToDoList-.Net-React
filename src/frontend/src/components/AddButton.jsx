197cfcb7b7c0f8afa21beee397f4a57d42f58b6c
Franco Balich <franco.balich@gmail.com>
2023-04-03 16:37:56 -0300

    Add links

--- src/frontend/src/components/AddButton.jsx
+++ src/frontend/src/components/AddButton.jsx
@@ -1,9 +1,14 @@
 import addBtn from  '../assets/addBtn.svg'
 import '../styles/AddButton.css'
+import { useNavigate } from 'react-router-dom'
 
 export const AddButton = () => {
+  const navigate = useNavigate()
+  const addOnClick=()=>{
+    navigate('/new', { replace: true })
+  }
   return (
-    <div className='addButton_container' >
+    <div className='addButton_container' onClick={addOnClick} >
       <img className='addButton' src={addBtn} alt='Icono de mas' />
     </div>
   )
--- src/frontend/src/pages/NewTodo.jsx
+++ src/frontend/src/pages/NewTodo.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from 'react-router-dom'
 import '../App.css'
 import { Title } from '../components'
 
 export const NewTodo = () => {
+  const navigate = useNavigate()
+  const cancelOnClick=()=>{
+    navigate('/', { replace: true })
+  }
+  const acceptOnClick=()=>{
+    navigate('/', { replace: true })
+  }
   return (
     <div className="App">
       <Title title="Nueva tarea"/>
@@ -11,8 +19,8 @@ export const NewTodo = () => {
         <input className="browser__input" type="text" placeholder="Fecha de la tarea"/>
       </div>
       <div className='buttonContainer'>
-        <input className="cancel__btn" type="button" value="Cancelar" />
-        <input className="browser__btn" type="button" value="Crear" />
+        <input className="cancel__btn" type="button" value="Cancelar" onClick={cancelOnClick} />
+        <input className="browser__btn" type="button" value="Crear" onClick={acceptOnClick}/>
       </div>
     </div>
   )
