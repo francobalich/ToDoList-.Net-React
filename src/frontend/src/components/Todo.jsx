e26c109275e656b1eb3e8a288a3160b6ae908029
Franco Balich <franco.balich@gmail.com>
2023-04-03 17:22:15 -0300

    Add styles complete todo

--- src/frontend/src/components/Todo.jsx
+++ src/frontend/src/components/Todo.jsx
@@ -1,22 +1,35 @@
 import '../styles/Todo.css'
 import deleteBtn from '../assets/delete.svg'
 import { toDoApi } from '../../api'
+import { useState } from 'react';
 
 export const Todo = ({id,name,state, description,date}) => {
-
+  const [checked, setChecked] = useState(state);
   const deleteOnClick = async()=>{
     await toDoApi.delete(`/${id}`)
+  }
+  const completeOnClick = async()=>{
+    setChecked(!checked);
+    let todo = {
+      id,
+      name,
+      date,
+      description,
+      state: checked
+    }
+    console.log(todo);
+    //await toDoApi.put(`/${id}`)
   }
   return (
     <div className="todoItem">
 
-      <img src={deleteBtn} alt='Delete button' onClick={deleteOnClick}></img>
+      <img src={deleteBtn} alt='Delete button' onClick={deleteOnClick} ></img>
 
-      <input type='checkbox' className='checkbox' />
+      <input type='checkbox' className='checkbox' onClick={completeOnClick}/>
       <div className="todo_info">
-        <p className="todo_title">{name}</p>
-        <p className="todo_time">{`${date.substring(0,10)}`}</p>
-        <p className="todo_description">{description}</p>
+        <p className={`todo_title ${!checked?"complete":""}`}>{name}</p>
+        <p className={`todo_time ${!checked?"complete":""}`}>{`${date.substring(0,10)}`}</p>
+        <p className={`todo_description ${!checked?"complete":""}`}>{description}</p>
       </div>
     </div>
   )
