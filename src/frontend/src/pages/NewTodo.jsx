fe5bcac89814739feb5d4887e9c7412355cdc250
Franco Balich <franco.balich@gmail.com>
2023-04-03 16:32:01 -0300

    Add styles

--- src/frontend/src/pages/NewTodo.jsx
+++ src/frontend/src/pages/NewTodo.jsx
@@ -1,9 +1,19 @@
 import '../App.css'
+import { Title } from '../components'
 
 export const NewTodo = () => {
   return (
-    <>
-    <input className="browser__input" type="text" placeholder="Ingrese el nombre de la tarea"/>
-    </>
+    <div className="App">
+      <Title title="Nueva tarea"/>
+      <div className='inputContainer'>
+        <input className="browser__input" type="text" placeholder="Titulo de la tarea"/>
+        <input className="browser__input" type="text" placeholder="Nombre de la tarea"/>
+        <input className="browser__input" type="text" placeholder="Fecha de la tarea"/>
+      </div>
+      <div className='buttonContainer'>
+        <input className="cancel__btn" type="button" value="Cancelar" />
+        <input className="browser__btn" type="button" value="Crear" />
+      </div>
+    </div>
   )
 }
