36706cc8b536e1a6c6fca0892cb5f4dd3e23abdf
Franco Balich <franco.balich@gmail.com>
2023-04-02 21:34:34 -0300

    Add toDo API

---
+++ src/frontend/api/toDoApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { getEnvVariables } from '../helper/getEnvVariables'
+const {VITE_API_URL} = getEnvVariables()
+
+const toDoApi = axios.create({
+    baseURL:VITE_API_URL
+})
+
+//Todo : Configurar interceptores
+/*
+toDoApi.interceptors.request.use(config=>{
+    config.headers={
+        ...config.headers,
+        'x-token':localStorage.getItem('token')
+    }
+    return config
+})*/
+
+
+export default toDoApi
