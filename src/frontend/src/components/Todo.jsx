03d547b4cd25f9b5fc54be5c7326c27c8cdcd0cf
Franco Balich <franco.balich@gmail.com>
2023-04-03 17:31:10 -0300

    Delete clg

--- src/frontend/src/components/Todo.jsx
+++ src/frontend/src/components/Todo.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 export const Todo = ({id,name,state, description,date}) => {
   const [checked, setChecked] = useState(state);
   const deleteOnClick = async()=>{
-    await toDoApi.delete(`/${id}`)
+   await toDoApi.delete(`/${id}`)
   }
   const completeOnClick = async()=>{
     setChecked(!checked);
@@ -17,16 +17,15 @@ export const Todo = ({id,name,state, description,date}) => {
       description,
       state: !checked
     }
-    console.log(todo);
     let resp=await toDoApi.put(`/`,todo)
-    console.log(resp);
+    //console.log(resp);
   }
   return (
     <div className="todoItem">
 
       <img src={deleteBtn} alt='Delete button' onClick={deleteOnClick} ></img>
 
-      <input type='checkbox' className='checkbox' checked={state} onClick={completeOnClick}/>
+      <input type='checkbox' className='checkbox' checked={state} onChange={completeOnClick}/>
       <div className="todo_info">
         <p className={`todo_title ${!checked?"complete":""}`}>{name}</p>
         <p className={`todo_time ${!checked?"complete":""}`}>{`${date.substring(0,10)}`}</p>
