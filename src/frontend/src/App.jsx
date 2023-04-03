653cc2e52eabc2bad84d1be78f45923363c24a04
Franco Balich <franco.balich@gmail.com>
2023-04-02 21:35:00 -0300

    add demo btn

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { toDoApi } from '../api'
 import './App.css'
 
 function App() {
+  const testApi = async ()=>{
+     let resp = await toDoApi.get()
+     console.log(resp.data);
+     return
+  }
   return (
     <div className="App">
-
+        <button onClick={testApi}>
+          haceme clic
+        </button>
     </div>
   )
 }
